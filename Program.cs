89693e2da58026a7a8dcfad8eec2b1a8ca53f699
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-09-18 15:28:19 -0300

    Trabalhando com if e else

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
-using dio_dotnet_fundamentos.Models;
-
 int quantidadeEmEstoque = 10;
 
-int quantidadeCompra = 4;
+int quantidadeCompra = 12;
 
 if (quantidadeEmEstoque >= quantidadeCompra)
 {
     Console.WriteLine("Venda realizada!");
+}
+else
+{
+    Console.WriteLine("Desculpe-nos! Não temos a quantidade desejada em estoque!");
 }
