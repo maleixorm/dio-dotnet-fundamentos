dc1c917361074c5c8efd49d73c09c605b229f3ff
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-09-18 15:13:58 -0300

    Modify main code

--- Program.cs
+++ Program.cs
@@ -1,32 +1,10 @@
-﻿using dio_dotnet_fundamentos.Models;
+using dio_dotnet_fundamentos.Models;
 
-Pessoa pessoa1 = new Pessoa();
+int quantidadeEmEstoque = 10;
 
-pessoa1.Nome = "Marcos";
-pessoa1.Idade = 37;
-pessoa1.Apresentar();
+int quantidadeCompra = 4;
 
-int a = 5;
-int b = 10;
-int c = a + b;
-
-c += 5;
-
-// Casting
-
-int d = Convert.ToInt32("30");
-int e = int.Parse("12");
-
-int inteiro = 5;
-string f = inteiro.ToString();
-
-// Cast implícito
-
-int g = 5;
-double h = g;
-
-Console.WriteLine(c);
-Console.WriteLine(d);
-Console.WriteLine(e);
-Console.WriteLine(f);
-Console.WriteLine(h);
+if (quantidadeEmEstoque >= quantidadeCompra)
+{
+    Console.WriteLine("Venda realizada!");
+}
