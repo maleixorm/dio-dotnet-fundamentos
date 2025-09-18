8d044c44f3b6bd7814375ec2352eef16a96b4f09
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-09-18 16:43:02 -0300

    Condicionais e switch case

--- Program.cs
+++ Program.cs
@@ -1,12 +1,38 @@
 int quantidadeEmEstoque = 10;
+int quantidadeCompra = 0;
+bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
 
-int quantidadeCompra = 12;
+Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+Console.WriteLine($"Quantidade de compra: {quantidadeCompra}");
+Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
 
-if (quantidadeEmEstoque >= quantidadeCompra)
+if (quantidadeCompra == 0)
+{
+    Console.WriteLine("Venda inválida!");
+}
+else if (possivelVenda)
 {
     Console.WriteLine("Venda realizada!");
 }
 else
 {
     Console.WriteLine("Desculpe-nos! Não temos a quantidade desejada em estoque!");
+}
+
+Console.Write("Digite uma letra: ");
+string letra = Console.ReadLine();
+
+switch (letra)
+{
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+        Console.WriteLine("Vogal");
+        break;
+
+    default:
+        Console.WriteLine("Não é uma vogal");
+        break;
 }
