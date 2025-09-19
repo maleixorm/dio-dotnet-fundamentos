8ed012e50288e9766da035778c5183746968108b
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-09-19 15:15:57 -0300

    Utilizando a classe calculadora para fazer operações matemáticas

--- Program.cs
+++ Program.cs
@@ -1,38 +1,8 @@
-int quantidadeEmEstoque = 10;
-int quantidadeCompra = 0;
-bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
+using dio_dotnet_fundamentos.Models;
 
-Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
-Console.WriteLine($"Quantidade de compra: {quantidadeCompra}");
-Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
+Calculadora calc = new Calculadora();
 
-if (quantidadeCompra == 0)
-{
-    Console.WriteLine("Venda inválida!");
-}
-else if (possivelVenda)
-{
-    Console.WriteLine("Venda realizada!");
-}
-else
-{
-    Console.WriteLine("Desculpe-nos! Não temos a quantidade desejada em estoque!");
-}
-
-Console.Write("Digite uma letra: ");
-string letra = Console.ReadLine();
-
-switch (letra)
-{
-    case "a":
-    case "e":
-    case "i":
-    case "o":
-    case "u":
-        Console.WriteLine("Vogal");
-        break;
-
-    default:
-        Console.WriteLine("Não é uma vogal");
-        break;
-}
+calc.Somar(10, 30);
+calc.Subtrair(30, 10);
+calc.Multiplicar(15, 30);
+calc.Dividir(20, 2);
