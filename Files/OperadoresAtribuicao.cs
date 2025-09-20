9131b47b27a12fde72417ace81f42b432c8c0ea7
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-09-20 20:34:41 -0300

    Create operadores de atribuiçao

---
+++ Files/OperadoresAtribuicao.cs
@@ -0,0 +1,32 @@
+﻿// using dio_dotnet_fundamentos.Models;
+
+// Pessoa pessoa1 = new Pessoa();
+
+// pessoa1.Nome = "Marcos";
+// pessoa1.Idade = 37;
+// pessoa1.Apresentar();
+
+// int a = 5;
+// int b = 10;
+// int c = a + b;
+
+// c += 5;
+
+// // Casting
+
+// int d = Convert.ToInt32("30");
+// int e = int.Parse("12");
+
+// int inteiro = 5;
+// string f = inteiro.ToString();
+
+// // Cast implícito
+
+// int g = 5;
+// double h = g;
+
+// Console.WriteLine(c);
+// Console.WriteLine(d);
+// Console.WriteLine(e);
+// Console.WriteLine(f);
+// Console.WriteLine(h);
