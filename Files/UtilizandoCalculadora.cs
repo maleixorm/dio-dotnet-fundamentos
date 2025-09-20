8fb5f63085495be741195b7f8af6549c4f5bd6f4
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-09-20 20:34:55 -0300

    Utilizando a classe calculadora

---
+++ Files/UtilizandoCalculadora.cs
@@ -0,0 +1,14 @@
+// using dio_dotnet_fundamentos.Models;
+
+// Calculadora calc = new Calculadora();
+
+// calc.Somar(10, 30);
+// calc.Subtrair(30, 10);
+// calc.Multiplicar(15, 30);
+// calc.Dividir(20, 2);
+// calc.Potencia(2, 5);
+
+// calc.Seno(30);
+// calc.Coseno(30);
+// calc.Tangente(30);
+// calc.RaizQuadrada(16);
