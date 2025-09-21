946dd998717da9d394d3b6a027cba16fba59bde3
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-09-20 21:08:09 -0300

    Trabalhando com do-while

---
+++ Files/DoWhile.cs
@@ -0,0 +1,10 @@
+// using dio_dotnet_fundamentos.Models;
+
+// int numero = 7;
+// int contador = 1;
+
+// do
+// {
+//     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+//     contador++;
+// } while (contador <= 10);
--- Program.cs
+++ Program.cs
@@ -3,8 +3,8 @@ using dio_dotnet_fundamentos.Models;
 int numero = 7;
 int contador = 1;
 
-while (contador <= 10)
+do
 {
     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
     contador++;
-}
+} while (contador <= 10);
