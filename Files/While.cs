6d064bf352ff264e5ce1e95479c6f3cfe62c0be6
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-09-20 21:07:53 -0300

    Trabalhando com while

---
+++ Files/While.cs
@@ -0,0 +1,10 @@
+// using dio_dotnet_fundamentos.Models;
+
+// int numero = 7;
+// int contador = 1;
+
+// while (contador <= 10)
+// {
+//     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+//     contador++;
+// }
