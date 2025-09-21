efaa6618dfb3b78e2f082ba7b9440341bc2a27d9
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-09-20 21:21:56 -0300

    Criando um menu interativo

--- Program.cs
+++ Program.cs
@@ -1,10 +1,37 @@
 using dio_dotnet_fundamentos.Models;
 
-int numero = 7;
-int contador = 1;
+string opcao;
+bool exibirMenu = true;
 
-do
+while (exibirMenu)
 {
-    Console.WriteLine($"{numero} x {contador} = {numero * contador}");
-    contador++;
-} while (contador <= 10);
+    Console.Clear();
+    Console.WriteLine("Digite  a sua opção:");
+    Console.WriteLine("1 - Cadastrar Cliente");
+    Console.WriteLine("2 - Buscar Cliente");
+    Console.WriteLine("3 - Apagar Cliente");
+    Console.WriteLine("4 - Encerrar");
+
+    opcao = Console.ReadLine();
+
+    switch (opcao)
+    {
+
+        case "1":
+            Console.WriteLine("Cadastro de Cliente");
+            break;
+        case "2":
+            Console.WriteLine("Busca de Cliente");
+            break;
+        case "3":
+            Console.WriteLine("Apagar Cliente");
+            break;
+        case "4":
+            Console.WriteLine("Encerrar");
+            exibirMenu = false;
+            break;
+        default:
+            Console.WriteLine("Opção Inválida");
+            break;
+    }
+}
