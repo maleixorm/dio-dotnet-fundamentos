35c8491890cf57dc09bed6ea1dbc76d3b86b16fb
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-09-22 16:49:55 -0300

    Redimensionando um Array

---
+++ Files/ArrayComForeach.cs
@@ -0,0 +1,12 @@
+// using dio_dotnet_fundamentos.Models;
+
+// int[] arrayInteiros = new int[4];
+// arrayInteiros[0] = 72;
+// arrayInteiros[1] = 64;
+// arrayInteiros[2] = 56;
+// arrayInteiros[3] = 48;
+
+// foreach (int valor in arrayInteiros)
+// {
+//     Console.WriteLine(valor);
+// }
--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 using dio_dotnet_fundamentos.Models;
 
-int[] arrayInteiros = new int[3];
+int[] arrayInteiros = new int[4];
 arrayInteiros[0] = 72;
 arrayInteiros[1] = 64;
 arrayInteiros[2] = 56;
+arrayInteiros[3] = 48;
 
-foreach (int valor in arrayInteiros)
+Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
+
+for (int i = 0; i < arrayInteiros.Length; i++)
 {
-    Console.WriteLine(valor);
+    Console.WriteLine($"Posição nº {i} - {arrayInteiros[i]}");
 }
