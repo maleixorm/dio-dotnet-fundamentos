d1d2389f73fddf71cdac39374f58fad6598e7a83
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-09-22 16:53:53 -0300

    Copiando um array

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ arrayInteiros[1] = 64;
 arrayInteiros[2] = 56;
 arrayInteiros[3] = 48;
 
-Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
+// Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
+int[] arrayInteirosDobrado = new int[arrayInteiros.Length * 2];
+Array.Copy(arrayInteiros, arrayInteirosDobrado, arrayInteiros.Length);
 
 for (int i = 0; i < arrayInteiros.Length; i++)
 {
