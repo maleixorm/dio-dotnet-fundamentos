fcb0b45421e481538eab6565af79d9d0cf053ccd
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-09-22 16:25:55 -0300

    Trabalhando com arrays

--- Program.cs
+++ Program.cs
@@ -1,37 +1,11 @@
 using dio_dotnet_fundamentos.Models;
 
-string opcao;
-bool exibirMenu = true;
+int[] arrayInteiros = new int[3];
+arrayInteiros[0] = 72;
+arrayInteiros[1] = 64;
+arrayInteiros[2] = 56;
 
-while (exibirMenu)
+for (int i = 0; i < arrayInteiros.Length; i++)
 {
-    Console.Clear();
-    Console.WriteLine("Digite  a sua opção:");
-    Console.WriteLine("1 - Cadastrar Cliente");
-    Console.WriteLine("2 - Buscar Cliente");
-    Console.WriteLine("3 - Apagar Cliente");
-    Console.WriteLine("4 - Encerrar");
-
-    opcao = Console.ReadLine();
-
-    switch (opcao)
-    {
-
-        case "1":
-            Console.WriteLine("Cadastro de Cliente");
-            break;
-        case "2":
-            Console.WriteLine("Busca de Cliente");
-            break;
-        case "3":
-            Console.WriteLine("Apagar Cliente");
-            break;
-        case "4":
-            Console.WriteLine("Encerrar");
-            exibirMenu = false;
-            break;
-        default:
-            Console.WriteLine("Opção Inválida");
-            break;
-    }
+    Console.WriteLine($"Posição nº {i} - {arrayInteiros[i]}");
 }
