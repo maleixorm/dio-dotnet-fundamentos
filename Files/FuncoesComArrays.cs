fddd87392f8b6f4360b0f27b4c2cf0636f72d671
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-09-22 16:57:38 -0300

    Trabalhando com as funções resize e copy

---
+++ Files/FuncoesComArrays.cs
@@ -0,0 +1,16 @@
+// using dio_dotnet_fundamentos.Models;
+
+// int[] arrayInteiros = new int[4];
+// arrayInteiros[0] = 72;
+// arrayInteiros[1] = 64;
+// arrayInteiros[2] = 56;
+// arrayInteiros[3] = 48;
+
+// // Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
+// int[] arrayInteirosDobrado = new int[arrayInteiros.Length * 2];
+// Array.Copy(arrayInteiros, arrayInteirosDobrado, arrayInteiros.Length);
+
+// for (int i = 0; i < arrayInteiros.Length; i++)
+// {
+//     Console.WriteLine($"Posição nº {i} - {arrayInteiros[i]}");
+// }
