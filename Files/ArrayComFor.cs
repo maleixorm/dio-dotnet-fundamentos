ac674b23871cc67b867cbc6f27d61e3ab3e01e78
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-09-22 16:27:49 -0300

    Preenchendo um array e percorrendo o mesmo com o For

---
+++ Files/ArrayComFor.cs
@@ -0,0 +1,11 @@
+// using dio_dotnet_fundamentos.Models;
+
+// int[] arrayInteiros = new int[3];
+// arrayInteiros[0] = 72;
+// arrayInteiros[1] = 64;
+// arrayInteiros[2] = 56;
+
+// for (int i = 0; i < arrayInteiros.Length; i++)
+// {
+//     Console.WriteLine($"Posição nº {i} - {arrayInteiros[i]}");
+// }
